0e8fce1d908653808795da561019c94737754dd0
VladB <VladB@VLAD>
2024-03-26 12:12:50 +0300

    Добавьте файлы проекта.

---
+++ First_Lab_Rab/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_Lab_Rab
+{
+    internal static class Test
+    {
+        public static void Main(string[] args)
+        {
+            
+            List<int> array = new List<int>() { 8, 1, 2, 3, 4, 5, 6};
+            List<int> otherArray = new List<int>() { 3, 2, 1, 8, 4};
+            List<int> thirdArray = new List<int>();
+            Set set = new Set(array);
+
+            Console.Write("Union элементов в массив ===  ");
+            set.union(otherArray);
+            set.print();
+
+            Console.Write("Нахождение индекса переменной ===  ");
+            Console.WriteLine(set.getIndexOf(45));
+
+            Console.Write("Проверка существования элемента в коллекции ===  ");
+            Console.WriteLine(set.exists(101));
+
+            Console.Write("Добавление элемента в коллекцию и вывод ===  ");
+            set.add(6);
+            set.print();
+
+            Console.Write("Удаление элемента по значению из коллекции ===  ");
+            set.remove(95);
+            set.print();
+
+            Console.Write("Удаление элемента по индексу из коллекции ===  ");
+            set.removeAt(2);
+            set.print();
+
+            Console.WriteLine();
+            set.Addition(otherArray, thirdArray);
+        }
+    }
+}
---
+++ First_Lab_Rab/Set.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_Lab_Rab
+{
+    public class Set
+    {
+
+        //public int n;
+        public List<int> list = new List<int>();
+        public Set(List<int> list) {  this.list = list; }
+
+
+        /// <summary>
+        /// метод для получения индекса элемента
+        /// </summary>
+        /// <param name="list"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        /*public Set(int n) 
+        {
+            this.n = n;
+        }*/
+        public int getIndexOf(int value)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] == value)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        /// <summary>
+        /// метод для проверки существования элемента в коллекции
+        /// </summary>
+        /// <param name="list"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool exists(int value)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] == value)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// метод для добавления элемента в массив
+        /// </summary>
+        /// <param name="list"></param>
+        /// <param name="value"></param>
+        public void add(int value)
+        {
+            if (!list.Contains(value))
+            {
+                list.Add(value);
+            }
+        }
+
+        /// <summary>
+        /// метод удаляющий элемент массива по значению
+        /// </summary>
+        /// <param name="list"></param>
+        /// <param name="value"></param>
+        public void remove(int value)
+        {
+            list.Remove(value);
+        }
+
+        /// <summary>
+        /// метод удаляющий элемент по индексу
+        /// </summary>
+        /// <param name="list"></param>
+        /// <param name="index"></param>
+        public void removeAt(int index)
+        {
+            list.RemoveAt(index);
+        }
+
+        /// <summary>
+        /// метод для красивого вывода в консоль
+        /// </summary>
+        /// <param name="list"></param>
+        public void print()
+        {
+            foreach (int item in list)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public void union(List<int> otherList)
+        {
+            foreach (int item in otherList)
+            {
+                if (!list.Contains(item))
+                {
+                    list.Add(item);
+                }
+            }
+        }
+
+        public void Addition(List<int> otherList, List<int> list3)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (otherList.Contains(list[i]))
+                {
+                    list3.Add(list[i]);
+                }
+            }
+            foreach(int item in list3)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
